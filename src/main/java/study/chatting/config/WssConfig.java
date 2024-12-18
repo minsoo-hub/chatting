3f4c1fc14c98ee9bccc89ae4935663bfda4516c4
naminsu <naminsoo1020@naver.com>
2024-12-18 23:27:45 +0900

    add(config): WebConfig cors 기본 설정 & WssConfig 기본 설정

--- src/main/java/study/chatting/config/WssConfig.java
+++ src/main/java/study/chatting/config/WssConfig.java
@@ -1,4 +1,19 @@
 package study.chatting.config;
 
-public class WssConfig {
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+@Configuration
+@EnableWebSocket
+@RequiredArgsConstructor
+public class WssConfig implements WebSocketConfigurer {
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(null,"/ws/v1/chat")
+                .setAllowedOrigins("*");
+    }
 }
